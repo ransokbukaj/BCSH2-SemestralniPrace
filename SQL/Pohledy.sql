804ab1464b65cdcdda5279a03d14da2c15482da4
OndrejHoracek <st72505@upce.cz>
2025-11-17 19:30:37 +0100

    Přidaní složky SQL a vytvoření 3 pohledů.

---
+++ SQL/Pohledy.sql
@@ -0,0 +1,53 @@
+CREATE OR REPLACE VIEW v_umelecka_dila_prehled AS
+SELECT
+    d.idumeleckedilo,
+    d.nazev  AS dilo_nazev,
+    d.typdila,
+    d.datumzverejneni,
+    v.nazev AS vystava_nazev,
+    p.cena  AS prodejni_cena,
+    LISTAGG(u.prijmeni || ' ' || u.jmeno, ', ')
+        WITHIN GROUP (ORDER BY u.prijmeni, u.jmeno) AS umelci
+FROM umelecka_dila d
+    LEFT JOIN vystavy v ON v.idvystava = d.idvystava
+    LEFT JOIN prodeje p ON p.idprodej = d.idprodej
+    LEFT JOIN umelci_umelecka_dila ud ON ud.idumeleckedilo = d.idumeleckedilo
+    LEFT JOIN umelci u ON u.idumelec = ud.idumelec
+GROUP BY
+    d.idumeleckedilo, d.nazev, d.typdila, d.datumzverejneni,
+    v.nazev, p.cena;
+
+
+CREATE OR REPLACE VIEW v_prodeje_prehled AS
+SELECT
+    pr.idprodej,
+    d.nazev AS dilo_nazev,
+    pr.cena,
+    pr.datumprodeje,
+    dp.nazev AS druh_platby,
+    k.jmeno || ' ' || k.prijmeni AS kupec_jmeno,
+    k.telefonicislo,
+    k.email,
+    a.ulice
+      || ' ' || a.cislopopisne
+      || NVL2(a.cisloorientacni, '/' || a.cisloorientacni, '') AS kupec_adresa,
+    po.psc,
+    po.obec
+FROM prodeje pr
+    JOIN umelecka_dila d ON d.idprodej = pr.idprodej
+    JOIN druhy_plateb dp ON dp.iddruhplatby = pr.iddruhplatby
+    JOIN kupci k ON k.idkupec = pr.idkupec
+    JOIN adresy a ON a.idadresa = k.idadresa
+    JOIN posty po ON po.idposta = a.idposta;
+
+
+CREATE OR REPLACE VIEW v_navstevy_statistika AS
+SELECT
+    v.nazev AS vystava_nazev,
+    n.datumnavstevy,
+    dn.nazev AS druh_navstevy,
+    COUNT(*) AS pocet
+FROM navstevy n
+    JOIN druhy_navstev dn ON dn.iddruhnavstevy = n.iddruhnavstevy
+    JOIN vystavy v ON v.idvystava = n.idvystava
+GROUP BY v.nazev, n.datumnavstevy, dn.nazev;
--- SemestralniPrace/DatabaseAccess/UserManager.cs
+++ SemestralniPrace/DatabaseAccess/UserManager.cs
@@ -1,4 +1,4 @@
-﻿using Entities.Account;
+using Entities.Account;
 using System;
 using System.Collections.Generic;
 using System.Linq;
