c288dd0c38e86aee61a59b3453fba1ef094f18dd
Ondřej Horáček <st72505@upce.cz>
2025-12-09 12:20:47 +0100

    Úprava hesel s Textbox na PasswordBox.

---
+++ SemestralniPrace/GUI/Helpers/PasswordBoxHelper.cs
@@ -0,0 +1,81 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace GUI.Helpers
+{
+    public static class PasswordBoxHelper
+    {
+        public static readonly DependencyProperty BoundPasswordProperty =
+            DependencyProperty.RegisterAttached(
+                "BoundPassword",
+                typeof(string),
+                typeof(PasswordBoxHelper),
+                new FrameworkPropertyMetadata(string.Empty, OnBoundPasswordChanged));
+
+        public static readonly DependencyProperty BindPasswordProperty =
+            DependencyProperty.RegisterAttached(
+                "BindPassword",
+                typeof(bool),
+                typeof(PasswordBoxHelper),
+                new PropertyMetadata(false, OnBindPasswordChanged));
+
+        private static readonly DependencyProperty IsUpdatingProperty =
+            DependencyProperty.RegisterAttached(
+                "IsUpdating",
+                typeof(bool),
+                typeof(PasswordBoxHelper));
+
+        public static void SetBindPassword(DependencyObject dp, bool value)
+            => dp.SetValue(BindPasswordProperty, value);
+
+        public static bool GetBindPassword(DependencyObject dp)
+            => (bool)dp.GetValue(BindPasswordProperty);
+
+        public static string GetBoundPassword(DependencyObject dp)
+            => (string)dp.GetValue(BoundPasswordProperty);
+
+        public static void SetBoundPassword(DependencyObject dp, string value)
+            => dp.SetValue(BoundPasswordProperty, value);
+
+        private static bool GetIsUpdating(DependencyObject dp)
+            => (bool)dp.GetValue(IsUpdatingProperty);
+
+        private static void SetIsUpdating(DependencyObject dp, bool value)
+            => dp.SetValue(IsUpdatingProperty, value);
+
+        private static void OnBoundPasswordChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is PasswordBox box)
+            {
+                box.PasswordChanged -= HandlePasswordChanged;
+
+                if (!GetIsUpdating(box))
+                    box.Password = e.NewValue as string ?? string.Empty;
+
+                box.PasswordChanged += HandlePasswordChanged;
+            }
+        }
+
+        private static void OnBindPasswordChanged(DependencyObject dp, DependencyPropertyChangedEventArgs e)
+        {
+            if (dp is PasswordBox box)
+            {
+                if ((bool)e.OldValue)
+                    box.PasswordChanged -= HandlePasswordChanged;
+
+                if ((bool)e.NewValue)
+                    box.PasswordChanged += HandlePasswordChanged;
+            }
+        }
+
+        private static void HandlePasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is PasswordBox box)
+            {
+                SetIsUpdating(box, true);
+                SetBoundPassword(box, box.Password);
+                SetIsUpdating(box, false);
+            }
+        }
+    }
+}
