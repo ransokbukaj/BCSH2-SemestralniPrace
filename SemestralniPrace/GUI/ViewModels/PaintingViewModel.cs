ec6bba65b0137a6a558c917622963b0a56992df2
Ondřej Horáček <st72505@upce.cz>
2025-12-09 15:55:18 +0100

    Funkční Malba.

--- SemestralniPrace/GUI/ViewModels/PaintingViewModel.cs
+++ SemestralniPrace/GUI/ViewModels/PaintingViewModel.cs
@@ -8,15 +8,25 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace GUI.ViewModels
 {
     public partial class PaintingViewModel : ObservableObject
     {
         private readonly PaintingRepository repository = new PaintingRepository();
+        private readonly CounterRepository counterRep = new CounterRepository();
+
+
 
         [ObservableProperty]
         private ObservableCollection<Painting> paintings = new();
+        [ObservableProperty]
+        private ObservableCollection<Counter> techniques = new();
+        [ObservableProperty]
+        private ObservableCollection<Counter> bases = new();
+
+
 
         [ObservableProperty]
         private Painting selectedPainting;
@@ -30,12 +40,18 @@ namespace GUI.ViewModels
         private void Load()
         {
             Paintings = new ObservableCollection<Painting>(repository.GetList());
+            Techniques = new ObservableCollection<Counter>(counterRep.GetTechniques());
+            Bases = new ObservableCollection<Counter>(counterRep.GetFoundations());
         }
 
         [RelayCommand]
         private void New()
         {
-            SelectedPainting = new Painting();
+            SelectedPainting = new Painting()
+            {
+                Base = new Counter(),
+                Technique = new Counter()
+            };
         }
 
         [RelayCommand]
@@ -43,6 +59,19 @@ namespace GUI.ViewModels
         {
             if (SelectedPainting == null)
                 return;
+
+
+            if (Bases != null)
+            {
+                // sjednotit instanci Post podle Id
+                SelectedPainting.Base = Bases.FirstOrDefault(p => p.Id == SelectedPainting.Base.Id);
+            }
+
+            if (Techniques != null)
+            {
+                // sjednotit instanci Post podle Id
+                SelectedPainting.Technique = Techniques.FirstOrDefault(p => p.Id == SelectedPainting.Technique.Id);
+            }
 
             repository.SaveItem(SelectedPainting);
             Load();
