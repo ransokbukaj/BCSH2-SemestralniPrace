680ec05ca3eb978318082f89183b9dc42a23cf90
Ondřej Horáček <st72505@upce.cz>
2025-12-10 11:49:41 +0100

    Nefunkční AttachentRepository.

--- SemestralniPrace/DatabaseAccess/AttachmentRepository.cs
+++ SemestralniPrace/DatabaseAccess/AttachmentRepository.cs
@@ -1,5 +1,6 @@
 ﻿using DatabaseAccess.Interface;
 using Entities;
+using Oracle.ManagedDataAccess.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +11,6 @@ namespace DatabaseAccess
 {
     public class AttachmentRepository : IAttachmentRepository
     {
-        public void DeleteItem(int id)
-        {
-            throw new NotImplementedException();
-        }
-
         public List<Attachment> GetList()
         {
             throw new NotImplementedException();
@@ -22,12 +18,59 @@ namespace DatabaseAccess
 
         public List<Attachment> GetListByArtPieceId(int id)
         {
-            throw new NotImplementedException();
+            var list = new List<Attachment>();
+            using (var command = ConnectionManager.Connection.CreateCommand())
+            {
+                command.CommandText = @"
+                    SELECT 
+                        idpriloha,
+                        soubor,
+                        typsouboru,
+                        nazevsouboru,
+                   
+                    FROM prilohy
+                    WHERE idumeleckedilo = :umeleckeId
+                    ORDER BY nazevsouboru";
+
+                var paramId = new OracleParameter
+                {
+                    ParameterName = "umeleckeId",
+                    OracleDbType = OracleDbType.Int32,
+                    Value = id
+                };
+                command.Parameters.Add(paramId);
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int idTemp = Convert.ToInt32(reader["idpriloha"]);
+                        long blobLength = reader.GetBytes( 0, 0,  null,  0, 0);
+                        byte[] fileTemp = new byte[blobLength];
+                        reader.GetBytes(0, 0, fileTemp, 0, fileTemp.Length);
+
+                        list.Add(new Attachment
+                        {
+                            Id = idTemp,
+                            File = fileTemp,
+                            FileType = reader["typsouboru"].ToString(),
+                            FileName = reader["nazevsouboru"].ToString()
+                        });
+                    }
+                }
+            }
+            return list;
         }
 
         public void SaveItem(Attachment attachment)
         {
             throw new NotImplementedException();
         }
+
+        public void DeleteItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
