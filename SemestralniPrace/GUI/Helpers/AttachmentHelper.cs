dffa3badd88a63f5650d2dfd4ebe2fe95c3297ff
Ondřej Horáček <st72505@upce.cz>
2025-12-12 11:39:52 +0100

    Komentář k AttachmentHelper.

--- SemestralniPrace/GUI/Helpers/AttachmentHelper.cs
+++ SemestralniPrace/GUI/Helpers/AttachmentHelper.cs
@@ -11,6 +11,11 @@ namespace GUI.Helpers
 {
     public static class AttachmentHelper
     {
+        /// <summary>
+        /// Metoda pro načtění ImageSource z pole byte.
+        /// </summary>
+        /// <param name="blobData">Pole bytů obsahujicí obrázek.</param>
+        /// <returns>ImageSource, zdroj dat pro vykreslení obrázku.</returns>
         public static ImageSource LoadImageSource(byte[] blobData)
         {
             using var ms = new MemoryStream(blobData);
